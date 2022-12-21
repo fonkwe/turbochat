dd056fb2ad722b75e84539961f3f4d4c504d0c22
fonkwe <williajeni@gmail.com>
2022-12-21 09:03:52 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :rooms 
+  root 'pages#home'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
+  get 'user/:id', to: 'users#show', as: 'user'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
